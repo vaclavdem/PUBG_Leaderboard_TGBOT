676b267d58ca8222962019cfc987d0ca1094fd43
Vatslav <85072668+vaclavdem@users.noreply.github.com>
2025-03-15 15:26:57 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,1 +1,43 @@
+import chicken_dinner.pubgapi
+from chicken_dinner.pubgapi import PUBGCore
+from chicken_dinner.pubgapi import PUBG
+from pubg_python import PUBG, Shard
+import pubg_python
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Enum, Table, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+import discord
+import os
+from aiogram.client.default import DefaultBotProperties
+from aiogram import Bot, Dispatcher, types
+from aiogram.enums import ParseMode
 
+# Подключение к MySQL
+DATABASE_URL = "mysql+mysqlconnector://Vatslav:Va_12345678@localhost/PUBG_db"
+engine = create_engine(DATABASE_URL)
+
+Base = declarative_base()
+
+telegram_bot = Bot(
+    token=tg_token,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
+
+dp = Dispatcher()
+
+intents = discord.Intents.default()
+intents.voice_states = True
+intents.guilds = True
+intents.members = True
+client = discord.Client(intents=intents)
+
+# Создание сессии
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# API PUBG
+api_1 = chicken_dinner.pubgapi.PUBG(api_key, "pc-eu")
+
+api_2 = pubg_python.PUBG(api_key, Shard.PC_EU)
